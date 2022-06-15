a317ba531176f3e36afb49ecc76a9e66852aa16d
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-06-15 17:28:51 -0300

    Virando Card

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let trava = false;
+
+const GifsParrots = [
+    "bobrossparrot.gif", 
+    "explodyparrot.gif", 
+    "fiestaparrot.gif", 
+    "metalparrot.gif", 
+    "revertitparrot.gif",
+    "tripletsparrot.gif",
+    "unicornparrot.gif"
+];
+
 while(trava === false){
     alert("Digite valores pares no intervalo de 4 a 14");
     const num = prompt("Digite o número de cartas que deseja jogar");
@@ -12,12 +23,21 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
+function virar(elemento){
+
+    let virar = elemento.children;
+    virar[0].classList.add("virarFace");
+    virar[1].classList.add("rodarFace");
+}
+
 function EmbaralharCartas(num){
 
     let vetorObjetos = [];
+    let cont=0;
 
     for(let i=0; i<num; i++){
-       vetorObjetos.push(`<div><img src="./assets/front.png"></div>`) ;
+       vetorObjetos.push(`<div class="card" onclick="virar(this);"><div class="front face"><img src="./assets/${GifsParrots[cont]}"></div><div class="back-face face"><img src="./assets/front.png"></div></div>`) ;
+       if((i+1)%2===0) cont++;
     }
     
     vetorObjetos.sort(comparador);
