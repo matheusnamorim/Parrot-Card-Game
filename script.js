fef3959ae0b3abdcc4bcc7fa911db2a32da6cb63
Matheus Amorim <matheusnavesamorim@gmail.com>
2022-06-19 18:58:31 -0300

    Virando cards errado e time de 1s

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let trava = false;
+let cont = 0;
+let num;
 
 const GifsParrots = [
     "bobrossparrot.gif", 
@@ -12,7 +14,7 @@ const GifsParrots = [
 
 while(trava === false){
     alert("Digite valores pares no intervalo de 4 a 14");
-    const num = prompt("Digite o número de cartas que deseja jogar");
+    num = prompt("Digite o número de cartas que deseja jogar");
     if((num > 3 && num < 15) && num%2===0){ 
         EmbaralharCartas(num);
         trava = true;
@@ -24,10 +26,44 @@ function comparador() {
 }
 
 function virar(elemento){
-
+    cont++;
     let virar = elemento.children;
+
     virar[0].classList.add("virarFace");
-    virar[1].classList.add("rodarFace");
+    virar[1].classList.add("rodarFace");    
+
+    testarPar();
+}
+
+function cartaRevelada(carta1, carta2){
+    carta1.classList.remove("virarFace");
+    carta1.classList.add("virarFaceRevelada");
+    carta2.classList.remove("virarFace");
+    carta2.classList.add("virarFaceRevelada");
+}
+
+function virarCarta(carta1Virar, carta1Rodar, carta2Virar, carta2Rodar){
+
+    setTimeout(function(){
+        carta1Virar.classList.remove("virarFace"); 
+        carta1Rodar.classList.remove("rodarFace");
+        carta2Virar.classList.remove("virarFace"); 
+        carta2Rodar.classList.remove("rodarFace");
+    }, 1000);
+}
+
+function testarPar(){
+    const cartaVirada = document.querySelectorAll(".virarFace");
+    if(cartaVirada.length === 2){
+        const numeracaoCarta1 = cartaVirada[0].parentNode.children;
+        const numeracaoCarta2 = cartaVirada[1].parentNode.children;
+        if(numeracaoCarta1[2].innerHTML === numeracaoCarta2[2].innerHTML){
+            cartaRevelada(cartaVirada[0], cartaVirada[1]);
+        }else{
+            
+            virarCarta(numeracaoCarta1[0], numeracaoCarta1[1], numeracaoCarta2[0], numeracaoCarta2[1]);
+        }
+    }
 }
 
 function EmbaralharCartas(num){
@@ -36,7 +72,7 @@ function EmbaralharCartas(num){
     let cont=0;
 
     for(let i=0; i<num; i++){
-       vetorObjetos.push(`<div class="card" onclick="virar(this);"><div class="front face"><img src="./assets/${GifsParrots[cont]}"></div><div class="back-face face"><img src="./assets/front.png"></div></div>`) ;
+       vetorObjetos.push(`<div class="card" onclick="virar(this);"><div class="front face"><img src="./assets/${GifsParrots[cont]}"></div><div class="back-face face"><img src="./assets/front.png"></div><p>${cont}</p></div>`) ;
        if((i+1)%2===0) cont++;
     }
     
